019ea2018a2bd12811f7f842561eef5bbb0497d1
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-13 18:42:30 +0530

    Create Queue and add elements

---
+++ src/com/bridgelabz/QueueRunner.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class QueueRunner {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<Integer>();
+        queue.enqueue((56));
+        queue.enqueue((70));
+        queue.enqueue((30));
+        queue.show();
+
+    }
+}
